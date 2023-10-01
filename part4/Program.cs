40261f7dd75b7227eebd647bbec3e6520c9bb5d7
SultanMurad78 <143870223+SultanMurad78@users.noreply.github.com>
2023-10-02 02:51:36 +0300

    Добавил папку HomeWork и сделал домашнее задание 2

--- part4/Program.cs
+++ part4/Program.cs
@@ -24,3 +24,84 @@
 // double d = Math.Sqrt(Math.Pow(x1-x2,2)+ Math.Pow(y1-y2,2));
 
 // Console.WriteLine(d);
+
+
+
+
+// Напишите программу, которая принимает на вход число (N) и
+//  выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// Console.WriteLine("Введите число");
+// int num= Convert.ToInt32(Console.ReadLine());
+
+// int i = 1;
+
+//  while(i<=num)
+//  {
+//     Console.WriteLine(Math.Pow(i,2));
+//     i=i+1;
+//  }
+
+
+
+// Console.WriteLine("Введите число");
+// int num= Convert.ToInt32(Console.ReadLine());
+
+// for(int i = 1; i<=num; i++)
+// {
+//     Console.WriteLine(Math.Pow(i,2));
+// }
+
+
+
+// какой то пример для самостоятельного разбора
+// using System;
+// //using System.Collections.Generic;
+// //using System.Linq;
+// //using System.Text;
+// //using System.Threading.Tasks;
+
+// namespace cSharp
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine(СonvertString() + " - сконвертированное число");
+//             Console.ReadKey();
+//         }
+
+//         private int СonvertString()
+//         {
+//             string userInputString;
+//             int userInputInt = 0;
+//             bool wasNumberConverted = false;
+
+//             Console.WriteLine("Введите строку для конвертации");
+
+//             while (wasNumberConverted == false)
+//             {
+//                 userInputString = Console.ReadLine();
+
+//                 if (int.TryParse(userInputString, out int ConvertedUserInput))
+//                 {
+//                     Console.WriteLine("Конвертация прошла успешно");
+//                     userInputInt = ConvertedUserInput;
+//                     wasNumberConverted = true;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine("Конвертация не удалась");
+//                     wasNumberConverted = false;
+//                 }
+//             }
+
+//             return userInputInt;
+//         }
+//     }
+// }
+
