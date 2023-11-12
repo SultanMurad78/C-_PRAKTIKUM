99d774a3aeec933c2d110ee1e9a0eb5707a703c5
SultanMurad78 <143870223+SultanMurad78@users.noreply.github.com>
2023-11-12 16:32:31 +0300

    добавил домашнее задание 6

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -39,3 +39,284 @@
 // if (num > 0 && num <= 5){Console.WriteLine("Увы, это рабочий день недели!");}
 // else if (num > 5 && num <= 7){Console.WriteLine("Ура! выходный день недели!");}
 // else {Console.WriteLine("Такого дня недели нет!");}
+
+//________________________________________________________________________________________________________________________________________________________________________________
+
+
+// Домашнее задание 5
+// ЗАДАЧА 1 
+
+// Напишите программу, которая подсчитывает количество четных элементов в массиве целых положительных трехзначных чисел и выводит результат на экран.
+
+// Программа должна иметь метод CountEvenElements, который принимает массив целых положительных трехзначных чисел и возвращает количество четных элементов в массиве.
+
+// Программа должна иметь метод PrintArray, который выводит элементы массива на экран.
+
+// При выводе элементы массива должны быть разделены символом табуляции "\t".
+
+// Если аргументы командной строки не переданы, программа использует массив по умолчанию, который содержит следующие значения: {100, 102, 105, 166, 283, 764, 300, 499, 133}.
+// Аргументы передаваемые в метод
+// '124, 378, 593, 821, 456'
+
+// На выходе
+// Массив:
+// 124     378     593     821     456 
+// Количество четных элементов: 3
+
+
+
+// using System;
+
+// public class Answer
+// {
+//     public static int CountEvenElements(int[] array)
+//     {
+//         int count = 0;
+//         foreach (int number in array)
+//         {
+//             if (number % 2 == 0)
+//             {
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+
+//     public static void PrintArray(int[] array)
+//     {
+//       for (int i = 0; i < array.Length; i++)
+//       {
+//         Console.Write(array[i]);
+//         if (i < array.Length - 1) // Проверяем, не последний ли элемент
+//         {
+//         Console.Write("\t"); // Добавляем табуляцию после всех элементов, кроме последнего
+//         }
+//       }
+//         Console.WriteLine();
+//     }
+
+//     public static void Main(string[] args)
+//     {
+//         int[] array;
+//         if (args.Length == 0)
+//         {
+//             array = new int[] { 100, 102, 105, 166, 283, 764, 300, 499, 133 };
+//         }
+//         else
+//         {
+//             string[] argStrings = args[0].Split(", ");
+//             array = new int[argStrings.Length];
+//             for (int i = 0; i < argStrings.Length; i++)
+//             {
+//                 if (int.TryParse(argStrings[i], out int number))
+//                 {
+//                     array[i] = number;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+//                     return;
+//                 }
+//             }
+//         }
+
+//         Console.WriteLine("Массив:");
+//         PrintArray(array);
+//         int evenCount = CountEvenElements(array);
+//         Console.WriteLine($"Количество четных элементов: {evenCount}");
+//     }
+// }
+
+
+
+
+
+// Задача 2. Сумма нечетных элементов массива
+
+// Напишите программу, которая находит сумму элементов с нечетными 
+// индексами в одномерном массиве целых чисел и выводит результат на экран.
+
+// Программа должна иметь метод SumOddElements, который принимает 
+// массив целых чисел и возвращает сумму элементов с нечетными индексами 
+// в массиве.
+
+// Программа должна иметь метод PrintArray, который выводит элементы 
+// массива на экран.
+
+// При выводе элементы массива должны быть разделены символом табуляции "\t".
+
+// Аргументы, передаваемые в метод/функцию:
+// '18, 76, 11'
+// На выходе:
+// 18      76      11  
+// Сумма нечетных элементов: 76
+
+// using System;
+
+// public class Answer
+// {
+//     public static int SumOddElements(int[] array)
+//     {
+//         int sum = 0;
+//         for (int i = 1; i < array.Length; i = i + 2)
+//         {
+//             sum += array[i];
+//         }
+//         return sum;
+//     }
+
+//     public static void PrintArray(int[] array)
+//     {
+//       for (int i = 0; i < array.Length; i++)
+//       {
+//         Console.Write(array[i]);
+//         if (i < array.Length - 1) // Проверяем, не последний ли элемент
+//         {
+//         Console.Write("\t"); // Добавляем табуляцию после всех элементов, кроме последнего
+//         }
+//       }
+//         Console.WriteLine();
+//     }
+
+
+//     public static void Main(string[] args)
+//     {
+//         int[] array;
+//         if (args.Length == 0)
+//         {
+//             // Если аргументы не переданы, используем массив по умолчанию
+//             array = new int[] { 12, 64, 28, 93, 35, 47, 6, 72, 58, 21 };
+//         }
+//         else
+//         {
+//             string[] argStrings = args[0].Split(", ");
+//             array = new int[argStrings.Length];
+//             for (int i = 0; i < argStrings.Length; i++)
+//             {
+//                 if (int.TryParse(argStrings[i], out int number))
+//                 {
+//                     array[i] = number;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+//                     return;
+//                 }
+//             }
+//         }
+
+//         PrintArray(array);
+//         int sumOdd = SumOddElements(array);
+//         Console.WriteLine($"Сумма нечетных элементов: {sumOdd}");
+//     }
+// }
+
+
+
+
+
+
+//Задание 3
+// Разница между максимальным и минимальным элементами массива
+// Напишите программу для работы с массивом вещественных чисел.
+// Реализуйте класс ArrayOperations, который содержит следующие статические
+//  методы:
+// FindMax(double[] array): Метод принимает на вход массив вещественных 
+// чисел array и возвращает максимальное число из массива.
+// FindMin(double[] array): Метод принимает на вход массив вещественных 
+// чисел array и возвращает минимальное число из массива.
+// CalcDifferenceBetweenMaxMin(double[] array): Метод принимает на вход
+// массив вещественных чисел array и возвращает разницу между
+// максимальным и минимальным числами в массиве.
+// PrintArray(double[] array): Метод для вывода массива на экран.
+// Он принимает на вход массив array и выводит его элементы с двумя 
+// знаками после запятой через табуляцию.
+// Аргументы, передаваемые в метод/функцию:
+
+// '3.17, 8.94, 2.36, 5.72, 0.85'
+// На выходе:
+// Массив:
+// 3.17    8.94    2.36    5.72    0.85    
+// Разность между максимальным и минимальным элементом = 8.09
+
+// using System;
+
+// public class Answer
+// {
+//     public static double FindMax(double[] array)
+//     {
+//         double max = array[0];
+//         for (int i = 1; i < array.Length; i++)
+//         {
+//             if (max < array[i])
+//             {
+//                 max = array[i];
+//             }
+//         }
+//         return max;
+//     }
+
+//     public static double FindMin(double[] array)
+//     {
+//         double min = array[0];
+//         for (int i = 1; i < array.Length; i++)
+//         {
+//             if (min > array[i])
+//             {
+//                 min = array[i];
+//             }
+//         }
+//         return min;
+//     }
+
+//     public static double CalcDifferenceBetweenMaxMin(double[] array)
+//     {
+//         return FindMax(array) - FindMin(array);
+//     }
+
+//     public static void PrintArray(double[] array)
+//     {
+//       for (int i = 0; i < array.Length; i++)
+//       {
+//         Console.Write(array[i]);
+//         if (i < array.Length - 1) // Проверяем, не последний ли элемент
+//         {
+//         Console.Write("\t"); // Добавляем табуляцию после всех элементов, кроме последнего
+//         }
+//       }
+//         Console.WriteLine();
+//     }
+
+//     public static void Main(string[] args)
+//     {
+//         double[] array;
+//         if (args.Length == 0)
+//         {
+//             array = new double[] { 3, 7.4, 22.3, 2, 78 };
+//         }
+//         else
+//         {
+//             // Иначе, парсим аргументы в массив чисел
+//             string[] argStrings = args[0].Split(", ");
+//             array = new double[argStrings.Length];
+//             for (int i = 0; i < argStrings.Length; i++)
+//             {
+//                 if (double.TryParse(argStrings[i], out double number))
+//                 {
+//                     array[i] = number;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+//                     return;
+//                 }
+//             }
+//         }
+
+//         Console.WriteLine("Массив:");
+//         PrintArray(array);
+//         double diff = CalcDifferenceBetweenMaxMin(array);
+//         Console.WriteLine($"Разность между максимальным и минимальным элементом = {diff:f2}");
+//     }
+// }
+
--- part4/Program.cs
+++ part4/Program.cs
@@ -105,3 +105,228 @@
 //     }
 // }
 
+// Задача 31: Задайте массив из 12 элементов, заполненный
+// случайными числами из промежутка [-9, 9]. Найдите сумму
+// отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма
+// положительных чисел равна 29, сумма отрицательных равна -20.
+
+// int[] array = new int[12];
+
+// int sumNum=0;
+// int sumNegNum=0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i]= new Random().Next(-9,10);
+// System.Console.Write(array[i]+ " ");
+
+// if(array[i]>0)
+// {
+// sumNum=sumNum+ array[i];
+// }
+// else
+// {
+// sumNegNum= sumNegNum +array[i];
+// }
+// }
+// System.Console.WriteLine("Сумма полож.= "+ sumNum+ " отриц. = "+ sumNegNum);
+
+
+// Задача 32: Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// int[] array = new int[] {3,-21,9,-2};
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i]= array[i] *-1;
+// System.Console.Write(array[i]+ " ");
+// }
+
+
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+// int[] array = new int[6];
+
+// Console.WriteLine("Введите число");
+// int num = int.Parse(Console.ReadLine());
+
+// bool isFind= false;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i]= new Random().Next(-1,3);
+// System.Console.Write(array[i]+ " ");
+
+// if(array[i]== num)
+// {
+// isFind=true;
+// }
+
+// }
+
+// if(isFind)
+// {
+// Console.Write("Да");
+// }
+// else
+// {
+// Console.Write("Нет");
+// }
+
+
+
+
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество
+// элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+// int[] array = new int[123];
+
+// int count=0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i]= new Random().Next(-1,150);
+// System.Console.Write(array[i]+ " ");
+
+// if(array[i]>9 && array[i]<100)
+// {
+// count++;
+// // count=count+1;
+// }
+// }
+// System.Console.WriteLine("Количество = "+ count);
+
+
+
+
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+// int[] array = new int[5];
+// int countElErray1= array.Length;
+// int[] array2 = new int[countElErray1/2 + countElErray1%2];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i]= new Random().Next(-1,10);
+// System.Console.Write(array[i]+ " ");
+// }
+
+// System.Console.WriteLine();
+
+// for (int i = 0; i < array2.Length; i++)
+// {
+// if(i == countElErray1-1-i)
+// {
+// array2[i]= array[i];
+// System.Console.Write(array2[i]+ " ");
+// break;
+// }
+
+// array2[i]= array[i] * array[countElErray1-1-i];
+// System.Console.Write(array2[i]+ " ");
+
+// }
+
+// using System;
+
+// public class Answer
+// {
+//     public static double FindMax(double[] array)
+//     {     // Введите свое решение ниже
+//        if (array == null) throw new ArgumentNullException(nameof(array));
+//        if (array.Length == 0) return 0;
+//        double maxValue = array[0];
+//           for (int i = 1; i < array.Length; i++)
+//           {
+//             if (maxValue < array[i]) maxValue = array[i];
+//           }
+//         return maxValue;          
+//     }
+
+//     public static double FindMin(double[] array)
+//     {     // Введите свое решение ниже
+//         if (array == null) throw new ArgumentNullException(nameof(array));
+//         if (array.Length == 0) return 0;
+
+//         double minValue = array[0];
+
+//         for(int i = 1; i < array.Length; i++)
+//         {
+//             if(minValue > array[i]) minValue = array[i];
+//         }
+//         return minValue;
+//     }
+
+//     public static double CalcDifferenceBetweenMaxMin(double[] array)
+//     {// Введите свое решение ниже
+
+//         return Math.Abs(FindMax(array) - FindMin(array));
+
+//     }
+
+//     public static void PrintArray(double[] array)
+//     {// Введите свое решение ниже
+//         foreach (var item in array)
+//         {
+//             Console.Write("{0:F2}", item);
+//             Console.Write("\t");
+//         }
+
+//         Console.WriteLine();
+//     }
+//  // Не удаляйте и не меняйте метод Main! 
+
+
+//     public static void Main(string[] args)
+//     {
+//         double[] array;
+//         if (args.Length == 0)
+//         {
+//             array = new double[] { 3, 7.4, 22.3, 2, 78 };
+//         }
+//         else
+//         {
+//             // Иначе, парсим аргументы в массив чисел
+//             string[] argStrings = args[0].Split(", ");
+//             array = new double[argStrings.Length];
+//             for (int i = 0; i < argStrings.Length; i++)
+//             {
+//                 if (double.TryParse(argStrings[i], out double number))
+//                 {
+//                     array[i] = number;
+//                 }
+//                 else
+//                 {
+//                     Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+//                     return;
+//                 }
+//             }
+//         }
+
+//         Console.WriteLine("Массив:");
+//         PrintArray(array);
+//         double diff = CalcDifferenceBetweenMaxMin(array);
+//         Console.WriteLine($"Разность между максимальным и минимальным элементом = {diff:f2}");
+//     }
+// }
+
+
+
