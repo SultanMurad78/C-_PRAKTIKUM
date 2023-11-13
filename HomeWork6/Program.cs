1e4fe72755151fdd6014761a9b6b283c70be7192
SultanMurad78 <143870223+SultanMurad78@users.noreply.github.com>
2023-11-13 09:57:32 +0300

    добавил задачи в семинары

--- HomeWork6/Program.cs
+++ HomeWork6/Program.cs
@@ -3,14 +3,57 @@
 // 0, 7, 8, -2, -2 -> 28
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Введите количество чисел для ввода: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int count = 0;
+// Console.WriteLine("Введите количество чисел для ввода: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int count = 0;
 
-for(int i = 0; i < N; i++)
+// for(int i = 0; i < N; i++)
+// {
+//     System.Console.WriteLine($"Введите {i+1}-е число: ");
+//     if(Convert.ToInt32(Console.ReadLine()) > 0) count++;
+// }
+
+// System.Console.WriteLine("Количество введенных положительных чисел = "+count);
+
+//______________________________________________________________________________________________________________________________________
+
+
+//Задача 43: Напишите программу, которая найдёт точку 
+//пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+    Console.WriteLine("Введите коэффициенты b1 и k1 для первой прямой:");
+    double b1 = Convert.ToDouble(Console.ReadLine()),
+    k1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите коэффициенты b2 и k2 для второй прямой:");
+    double b2 = Convert.ToDouble(Console.ReadLine()),
+    k2 = Convert.ToDouble(Console.ReadLine());
+    double x = 0;
+    double y = 0;
+       
+    if (GetPoint())
+    {
+        Console.WriteLine($"Координаты точки пересечения: ({x}, {y})");
+    }
+    else
+    {
+        Console.WriteLine("Прямые не пересекаются.");
+    }
+
+bool GetPoint()
 {
-    System.Console.WriteLine($"Введите {i+1}-е число: ");
-    if(Convert.ToInt32(Console.ReadLine()) > 0) count++;
+   
+    if (k1 == k2)
+    {
+         return false;
+    }     
+    else 
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k2 * x + b2;
+        return true;
+    }
 }
-
-System.Console.WriteLine("Количество введенных положительных чисел = "+count);
--- part4/Program.cs
+++ part4/Program.cs
@@ -330,3 +330,249 @@
 
 
 
+// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку массива.
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] GetArray (int m, int n)
+// {
+// int [,] array = new int[m,n];
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+// for (int j = 0; j < array.GetLength(1); j++)
+// {
+// array[i,j] = new Random().Next(0,10);
+// }
+// }
+
+// return array;
+// }
+
+// void PrintArray(int[,] inputArray)
+// {
+// for (int i = 0; i < inputArray.GetLength(0); i++)
+// {
+// for (int j = 0; j < inputArray.GetLength(1); j++)
+// {
+// Console.Write(inputArray[i,j]);
+// }
+// Console.WriteLine();
+// }
+// }
+
+// void ChangeArray(int[,] matrix)
+// {
+// int rowsCount = matrix.GetLength(0)-1;
+
+// for (int i = 0; i < matrix.GetLength(1); i++)
+// {
+// int temp = matrix[0,i];
+// matrix[0,i] =matrix[rowsCount,i];
+// matrix[rowsCount,i]= temp;
+// }
+// }
+
+// int[,] array2= GetArray(rows,columns);
+// PrintArray(array2);
+// ChangeArray(array2);
+// System.Console.WriteLine();
+// PrintArray(array2);
+
+
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет
+// строки на столбцы. В случае, если это невозможно, программа должна
+// вывести сообщение для пользователя.
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// int [,] array = new int[rows,rows];
+// int [,] array2 = new int[rows,rows];
+
+// int[,] GetArray (int [,] array)
+// {
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+// for (int j = 0; j < array.GetLength(1); j++)
+// {
+// array[i,j] = new Random().Next(0,10);
+// }
+// }
+
+// return array;
+// }
+
+// void PrintArray(int[,] inputArray)
+// {
+// for (int i = 0; i < inputArray.GetLength(0); i++)
+// {
+// for (int j = 0; j < inputArray.GetLength(1); j++)
+// {
+// Console.Write(inputArray[i,j]);
+// }
+// Console.WriteLine();
+// }
+// }
+
+// void ChangeArray(int[,] matrix,int[,] matrix2 )
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix2[j,i]= matrix[i,j];
+// }
+// }
+// }
+
+// GetArray(array);
+// PrintArray(array);
+// ChangeArray(array,array2);
+// System.Console.WriteLine();
+// PrintArray(array2);
+
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет
+// строки на столбцы. В случае, если это невозможно, программа должна
+// вывести сообщение для пользователя.
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// if(rows != columns)
+// {
+// System.Console.WriteLine("Введите правильные числа");
+// return;
+// }
+
+// int [,] array = new int[rows,columns];
+// int [,] array2 = new int[rows,columns];
+
+// int[,] GetArray (int [,] array)
+// {
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+// for (int j = 0; j < array.GetLength(1); j++)
+// {
+// array[i,j] = new Random().Next(0,10);
+// }
+// }
+
+// return array;
+// }
+
+// void PrintArray(int[,] inputArray)
+// {
+// for (int i = 0; i < inputArray.GetLength(0); i++)
+// {
+// for (int j = 0; j < inputArray.GetLength(1); j++)
+// {
+// Console.Write(inputArray[i,j]);
+// }
+// Console.WriteLine();
+// }
+// }
+
+// void ChangeArray(int[,] matrix,int[,] matrix2 )
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// matrix2[j,i]= matrix[i,j];
+// }
+// }
+// }
+
+// GetArray(array);
+// PrintArray(array);
+// ChangeArray(array,array2);
+// System.Console.WriteLine();
+// PrintArray(array2);
+
+
+
+// Задача 59: Задайте двумерный массив из целых чисел. Напишите программу,
+// которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим
+// следующий массив:
+
+
+// Console.WriteLine("Введите размероность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите размероность массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int minValue= int.MaxValue;
+// int minRows=0;
+// int minColumns= 0;
+
+// int[,] GetArray (int m, int n)
+// {
+// int [,] array = new int[m,n];
+
+// for (int i = 0; i < array.GetLength(0); i++)
+// {
+// for (int j = 0; j < array.GetLength(1); j++)
+// {
+// array[i,j] = new Random().Next(0,10);
+
+// if(array[i,j] <minValue)
+// {
+// minValue = array[i,j];
+// minRows= i;
+// minColumns = j;
+// }
+// }
+// }
+
+// return array;
+// }
+
+// void PrintArray(int[,] inputArray)
+// {
+// for (int i = 0; i < inputArray.GetLength(0); i++)
+// {
+// for (int j = 0; j < inputArray.GetLength(1); j++)
+// {
+// Console.Write(inputArray[i,j]);
+// }
+// Console.WriteLine();
+// }
+// }
+
+// void ChangeArray(int[,] matrix)
+// {
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// if (minRows!= i && minColumns!=j)
+// {
+// Console.Write(matrix[i,j]);
+// }
+// }
+// Console.WriteLine();
+// }
+// }
+
+// int[,] array2= GetArray(rows,columns);
+// PrintArray(array2);
+// System.Console.WriteLine();
+// ChangeArray(array2);
